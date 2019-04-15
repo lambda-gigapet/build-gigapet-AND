b04e8ffdfed3f7b222fd616e98a75109c9885d4f
BL-CoderOvertime <brandonlively@live.com>
2019-04-15 17:20:02 -0400

    Gigapet Model complete

--- app/src/main/java/com/example/gigapet/Gigapet.java
+++ app/src/main/java/com/example/gigapet/Gigapet.java
@@ -1,6 +1,50 @@
 package com.example.gigapet;
 
 public class Gigapet {
+private String name;
+private String uniqueId;
+private int exp;
+private int repoId;
+private int[] imageResource;
 
+    public Gigapet(String name, String uniqueId) {
+        this.name = name;
+        this.uniqueId = uniqueId;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    public void setRepoId(int repoId) {
+        this.repoId = repoId;
+    }
+
+    public void setImageResource(int[] imageResource) {
+        this.imageResource = imageResource;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUniqueId() {
+        return uniqueId;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public int getRepoId() {
+        return repoId;
+    }
+
+    public int[] getImageResource() {
+        return imageResource;
+    }
 }
