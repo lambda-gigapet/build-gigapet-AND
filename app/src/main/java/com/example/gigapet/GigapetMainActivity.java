3ecabcec6df70ffbd9bb27dff34c0da8ec8daf83
BL-CoderOvertime <brandonlively@live.com>
2019-04-15 15:53:24 -0400

    setup basic layout of parentMainActivity fragments.

--- app/src/main/java/com/example/gigapet/GigapetMainActivity.java
+++ app/src/main/java/com/example/gigapet/GigapetMainActivity.java
@@ -1,7 +1,10 @@
 package com.example.gigapet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class GigapetMainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class GigapetMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gigapet_main);
+
+        ImageView iv = findViewById(R.id.iv_options_sidebar_button);
+
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ParentMainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
