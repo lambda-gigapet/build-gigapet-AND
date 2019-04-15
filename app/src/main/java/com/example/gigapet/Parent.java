09e2da659e4fdb7e4299256047ad1f8bffda9136
BL-CoderOvertime <brandonlively@live.com>
2019-04-15 17:47:27 -0400

    Parent Model Complete

--- app/src/main/java/com/example/gigapet/Parent.java
+++ app/src/main/java/com/example/gigapet/Parent.java
@@ -1,4 +1,51 @@
 package com.example.gigapet;
 
+import java.util.ArrayList;
+
 public class Parent {
+    private ArrayList<Child> children;
+    private int id;
+    private String token;
+    private String name;
+
+    public Parent(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void addChild(Child child){
+        children.add(child);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public ArrayList<Child> getChildren() {
+        return children;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void removeChild(int position){
+        children.remove(position);
+    }
 }
