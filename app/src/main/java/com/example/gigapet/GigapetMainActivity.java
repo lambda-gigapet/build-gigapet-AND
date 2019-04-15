e14712e6d8eb0022a71b96950d90f0adce49e74e
BL-CoderOvertime <brandonlively@live.com>
2019-04-15 13:54:33 -0400

    Setup basic layout of GigapetMainActivity, and created NetworkAdapter class

--- app/src/main/java/com/example/gigapet/GigapetMainActivity.java
+++ app/src/main/java/com/example/gigapet/GigapetMainActivity.java
@@ -3,11 +3,11 @@ package com.example.gigapet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class GigapetMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.gigapet_main);
     }
 }
